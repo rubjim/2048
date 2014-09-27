57d2cbfdf789cb4b6ac92b90c6f6879033664841
rubjim <rubji91@gmail.com>
2014-09-27 03:45:10 -0400

    Terminado con el bug de que agrega numero igual si me muevo hacia una direccion invalida

--- 2048.c
+++ 2048.c
@@ -10,6 +10,7 @@
 #define UP 'w'
 #define DOWN 's'
 #define NEW_NUMBER 2
+#define WIN 2048
 
 //Definicion de las funciones // 
 void initializeBoard();		//Inicializa el tablero.
@@ -18,32 +19,26 @@ void placeNewNumber();		//Funcion que coloca un numero en un lugar libre.
 int checkForFreeSpaces();	//Funcion que retorna 1 si existe al menos un lugar vacio, 0 si no.
 char movement();			//Funcion que mueve y retorna que movimiento se introdujo.
 int gameOver();				//Determina si el juego finalizo.
-int checkToJoin(char);		//Revisa de acuerdo al movimiento si hay fichas para unir y retorna el valor de las fichas unidas.
+int checkToJoin(char,int);		//Revisa de acuerdo al movimiento si hay fichas para unir y retorna el valor de las fichas unidas.
 
 int board[BOARD_SIZE][BOARD_SIZE];
 
 int main(){
 	int points = 0;
 	int winOrLose = 0;
-	char c;
 	initializeBoard();
-	// while(!(winOrLose = gameOver())){
-	// 	points += checkToJoin(movement());
-	// 	placeNewNumber();
-	// 	printBoard();
-	// 	printf("Puntos: %d\n",points);
-	// }
-	// if (winOrLose == 1)
-	// 	printf("Has Ganado!\n");
-	// else
-	// 	printf("Has Perdido!\n");
 	printBoard();
-	while(1){
-		c = movement();
+	while(!(winOrLose = gameOver())){
+		printf("Introduzca su movimiento:");
+		points += checkToJoin(movement(),0);
 		placeNewNumber();
 		printBoard();
+		printf("Puntos: %d\n",points);
 	}
-	
+	if (winOrLose == 1)
+		printf("Has Ganado!\n");
+	else
+		printf("Has Perdido!\n");	
 	return 0;
 }	
 
@@ -90,17 +85,14 @@ void placeNewNumber(){
 }
 
 int checkForFreeSpaces(){
-	int i,j,freeSpace = 0;
+	int i,j,flag = 0;
 	for(i = 0; i < BOARD_SIZE; i++){
 		for(j = 0; j < BOARD_SIZE; j++)
 			if (board[i][j] == EMPTY){
-				freeSpace = 1;
-				break;
+				flag = 1;
 			}
-		if (freeSpace)
-			break;
 	}
-	return freeSpace;
+	return flag;
 }
 
 char movement(){
@@ -183,4 +175,117 @@ char movement(){
 			break;//END DEFAULT
 	}
 	return move;
+}
+
+int checkToJoin(char move,int checkIf){
+	int i,j,flag,aux,points = 0, joined;//flag es para seguir moviendo hasta el tope.
+	switch (move){
+		case RIGHT:
+			for(i = 0; i < BOARD_SIZE; i++){
+				joined = 0;
+				for(j = BOARD_SIZE-1; j > 0; j--){
+					if(board[i][j] == board[i][j-1] && !joined){
+						if(checkIf)
+							return 1;
+						board[i][j] += board[i][j-1];
+						board[i][j-1] = EMPTY;
+						points += board[i][j];
+						joined = 1;
+					}
+					if(board[i][j] == EMPTY && board[i][j-1] != EMPTY){
+						aux = board[i][j];
+						board[i][j] = board[i][j-1];
+						board[i][j-1] = aux;
+					}
+				}
+			}
+			break;//END CASE RIGHT
+		case LEFT:
+			for(i = 0; i < BOARD_SIZE; i++){
+				joined = 0;
+				for(j = 0; j < BOARD_SIZE - 1; j++){
+					if(board[i][j] == board[i][j+1] && !joined){
+						if(checkIf)
+							return 1;
+						board[i][j] += board[i][j+1];
+						board[i][j+1] = EMPTY;
+						points += board[i][j];
+						joined = 1;
+					}
+					if(board[i][j] == EMPTY && board[i][j+1] != EMPTY){
+						aux = board[i][j];
+						board[i][j] = board[i][j+1];
+						board[i][j+1] = aux;
+					}
+				}
+			}
+			break;//END CASE LEFT
+		case UP:
+			for(j = 0; j < BOARD_SIZE; j++){
+				joined = 0;
+				for(i = 0; i < BOARD_SIZE - 1; i++){
+					if(board[i][j] == board[i+1][j] && !joined){
+						if(checkIf)
+							return 1;
+						board[i][j] += board[i+1][j];
+						board[i+1][j] = EMPTY;
+						points += board[i][j];
+						joined = 1;
+					}
+					if(board[i][j] == EMPTY && board[i+1][j] != EMPTY){
+						aux = board[i][j];
+						board[i][j] = board[i+1][j];
+						board[i+1][j] = aux;
+						flag = 1;
+					}
+				}
+			}
+			break;//END CASE UP
+		case DOWN:
+			for(j = 0; j < BOARD_SIZE; j++){
+				joined = 0;
+				for(i = BOARD_SIZE - 1; i > 0 ; i--){
+					if(board[i][j] == board[i-1][j] && !joined){
+						if(checkIf)
+							return 1;
+						board[i][j] += board[i-1][j];
+						board[i-1][j] = EMPTY;
+						points += board[i][j];
+						joined = 1;
+					}
+					if(board[i][j] == EMPTY && board[i-1][j] != EMPTY){
+						aux = board[i][j];
+						board[i][j] = board[i-1][j];
+						board[i-1][j] = aux;
+						flag = 1;
+					}
+				}
+			}
+			break;//END CASE DOWN
+		default:
+			printf("No se encontro uniones en esa direccion\n");
+			break;//END DEFAULT
+	}
+	return points;
+}
+
+int gameOver(){
+	int i,j,lost = 0;
+	for(i = 0; i < BOARD_SIZE; i++){
+		for(j = 0; j < BOARD_SIZE - 1; j++){
+			if (board[i][j] == WIN)
+				return 1;
+		}
+	}
+	if (checkForFreeSpaces())
+		return 0;
+	else{
+		lost = lost + checkToJoin(UP,1) + checkToJoin(DOWN,1) + checkToJoin(LEFT,1) + checkToJoin(RIGHT,1);
+		
+		if(!lost)
+			return 2;
+		else 
+			return 0;
+	}
+
 }
